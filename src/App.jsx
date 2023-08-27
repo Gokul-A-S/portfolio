81e334e6a9952380c921625d15c369b36300d416
Gokul-A-S <gokuld271@gmail.com>
2023-08-27 21:14:09 +0530

    27/8

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import './App.css'
 import Navbar from './components/Navbar'
 import Main from './pages/Main'
+import Menu from './components/Menu'
 
 function App() {
 
   return (
-  <><Navbar />
-    <Main />
-    </>  
+    <><Navbar />
+      <Menu />
+      <Main />
+    </>
   )
 
 }
---
+++ src/components/Menu.jsx
@@ -0,0 +1,23 @@
+const Menu=()=>{
+    const toggleMenu=()=>{
+    }
+    return(
+        <div className="hamburger-nav">
+            <div className="logo">Gokul Das</div>
+            <div className="hamburger-menu">
+                <div className="hamburger-icon" onClick={toggleMenu()}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div className="menu-links">
+                <li><a href='#' onClick={toggleMenu()}>About</a></li>
+                <li><a href='#' onClick={toggleMenu()}>Contact</a></li>
+                <li><a href='#' onClick={toggleMenu()}>Skills</a></li>
+                <li><a href='#' onClick={toggleMenu()}>Projects</a></li>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Menu
