1c61050eff30374c0a4a6e7e3cc1b5e66409e4ec
Gokul-A-S <gokuld271@gmail.com>
2023-08-27 21:42:03 +0530

    27/8

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
 const Navbar=()=>{
     return(
         <nav id='desktop-nav'>
-           <div class='logo'>Gokul Das</div>
+           <div className='logo'>Gokul Das</div>
         <div >
-            <ul class='nav-links'>
+            <ul className='nav-links'>
                 <li><a href='#'>About</a></li>
                 <li><a href='#'>Contact</a></li>
                 <li><a href='#'>Skills</a></li>
