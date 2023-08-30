4c19a3fa608158e13823be5a645471b8635d34bf
Gokul-A-S <gokuld271@gmail.com>
2023-08-30 22:32:38 +0530

    30/8

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,20 +1,29 @@
+import { useRef } from "react"
 const Menu = () => {
+    const menuRef=useRef(null)
+    const iconRef=useRef(null)
+
     const toggleMenu = () => {
+        const menu=menuRef.current
+        const icon=iconRef.current
+        menu.classList.toggle('open')
+        icon.classList.toggle('open')
+
     }
     return (
         <nav id="hamburger-nav">
             <div className="logo">Gokul Das</div>
             <div className="hamburger-menu">
-                <div className="hamburger-icon" onClick={toggleMenu()}>
+                <div className="hamburger-icon" onClick={toggleMenu} ref={iconRef}>
                     <span></span>
                     <span></span>
                     <span></span>
                 </div>
-                <div className="menu-links">
-                    <li><a href='#' onClick={toggleMenu()}>About</a></li>
-                    <li><a href='#' onClick={toggleMenu()}>Contact</a></li>
-                    <li><a href='#' onClick={toggleMenu()}>Skills</a></li>
-                    <li><a href='#' onClick={toggleMenu()}>Projects</a></li>
+                <div className="menu-links" ref={menuRef}>
+                    <li><a href='#' onClick={toggleMenu}>About</a></li>
+                    <li><a href='#' onClick={toggleMenu}>Contact</a></li>
+                    <li><a href='#' onClick={toggleMenu}>Skills</a></li>
+                    <li><a href='#' onClick={toggleMenu}>Projects</a></li>
                 </div>
             </div>
         </nav>
