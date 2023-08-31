0e5782eee42c4cef7d2bfad4f67d397a99256d77
Gokul-A-S <gokuld271@gmail.com>
2023-08-31 22:40:44 +0530

    modified:   src/index.css 	modified:   src/pages/Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,21 +4,22 @@ import github from '../assets/github.png'
 const Profile = () => {
     return (
         <section id="profile">
-            <div className="section_pic_container">
+            <div className="section_pic-container">
                 <img src={profile} alt='Gokul' />
             </div>
-            <div className='section_text'>
-                <p className='section_text_p1'>Hello, I'm</p>
-                <h1 className='title'>Gokul Das</h1>
-                <p className='section_text_p2'>Software Developer</p>
-            </div>
-            <div className='btn-container'>
-                <button className='btn btn-color-2' onClick={()=>{window.open('./resume.pdf')}}>Download Resume</button>
-                <button className='btn btn-color-1' onClick={()=>{window.location.href='./#contact'}}>Contact</button>
-            </div>
-            <div id='socials-container'>
-                <img src={linkedin} alt='My LinkedIn Profile' onClick={()=>{window.location.href='https://www.linkedin.com/in/gokul271/'}} />
-                <img src={github} alt='My Github Profile' onClick={()=>{window.location.href='https://github.com/Gokul-A-S'}} />
+                <div className='section_text'>
+                    <p className='section_text_p1'>Hello, I'm</p>
+                    <h1 className='title'>Gokul Das</h1>
+                    <p className='section_text_p2'>Software Developer</p>
+                
+                <div className='btn-container'>
+                    <button className='btn btn-color-2' onClick={() => { window.open('./resume.pdf') }}>Download Resume</button>
+                    <button className='btn btn-color-1' onClick={() => { window.location.href = './#contact' }}>Contact</button>
+                </div>
+                <div id='socials-container'>
+                    <img src={linkedin} className='icon' alt='My LinkedIn Profile' onClick={() => { window.location.href = 'https://www.linkedin.com/in/gokul271/' }} />
+                    <img src={github} className='icon' alt='My Github Profile' onClick={() => { window.location.href = 'https://github.com/Gokul-A-S' }} />
+                </div>
             </div>
         </section>
     )
